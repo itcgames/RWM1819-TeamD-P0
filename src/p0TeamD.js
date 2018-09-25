5e052d87ca306506f047cd3a2d63efb70ee71a09
Rafael Plugge <tini.shadow@email.com>
2018-09-25 13:18:29 +0100

    Added Modulo3 submodule.

--- src/p0TeamD.js
+++ src/p0TeamD.js
@@ -1,7 +1,7 @@
 /*! p0TeamD v0.0.0 - MIT license */
 'use strict';
 
-var p0TeamD = function () {
-  // your code goes here
-    return Cube(4);
+var p0TeamD = function (num) {
+    var result = Cube(num);
+    return result;
 }
