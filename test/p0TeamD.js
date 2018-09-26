2cec86d488d222c04a63af029ddaac9fd6281ff2
KDaryl <c00207452@itcarlow.ie>
2018-09-26 09:32:19 +0100

    Added running total

--- test/p0TeamD.js
+++ test/p0TeamD.js
@@ -18,7 +18,7 @@ describe('p0TeamD()', function () {
 
     it('Modulo First 3,9,5,2,6,1 of 0,0,2,2,0,1', function() {
       expect(Modulo_First([3,9,5,2,6,1])).to.eql([0,0,2,2,0,1]);
-      
+
     })
 
     // Modulo_3 testing
@@ -33,5 +33,19 @@ describe('p0TeamD()', function () {
     });
     it("Modulo_3 of -5 is -2", function() {
         expect(Modulo_3(-5)).to.equal(-2);
+    });
+
+    // Running total tests
+    it("running_total exists?", function () {
+        expect(running_total).to.be.a("function");
+    });
+
+    it("The running total of 1,2,3,4 & 5 should equal 15", function() {
+        for(var val = 1; val < 5; val++)
+          running_total(val);
+        expect(running_total(5)).to.equal(15);
+    });
+    it("Add 30 to the running total, should equal 45", function() {
+        expect(running_total(30)).to.equal(45);
     });
 });
